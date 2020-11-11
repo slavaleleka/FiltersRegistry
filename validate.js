6364aec38e5f46e7af82c95b0e964b6d69ba235d
Slava Leleka <v.leleka@adguard.com>
2020-11-12 00:21:33 +0300

    add locales validator

--- validate.js
+++ validate.js
@@ -7,14 +7,21 @@ if (!platforms) {
     platforms = './platforms';
 }
 
+const LOCALES_DIR_PATH = './locales';
+
 const FILTERS_REQUIRED_AMOUNT = 80;
 
 const path = require('path');
 const compiler = require("adguard-filters-compiler");
 
 const platformsPath = path.join(__dirname, platforms);
-
 const validationResult = compiler.validateJSONSchema(platformsPath, FILTERS_REQUIRED_AMOUNT);
 if (!validationResult) {
     throw new Error('Invalid filters json');
+}
+
+const localesDirPath = path.join(__dirname, LOCALES_DIR_PATH);
+const localesValidationResult = compiler.validateLocales(localesDirPath);
+if (localesValidationResult.length !== 0) {
+    throw new Error('Invalid locales messages');
 }
